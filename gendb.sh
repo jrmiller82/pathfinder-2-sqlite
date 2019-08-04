f72b84f77b4a70918ebb33d49067659e900b5033
James R Miller <james.ryland.miller@gmail.com>
2019-08-03 22:21:47 -0500

    add some script output on gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -1,9 +1,17 @@
 rm pf2.db
+echo 'loading schema'
+echo 'schema/anscestries.sql'
 sqlite3 pf2.db < schema/ancestries.sql
+echo 'loading data'
+echo 'data/visions.sql'
 sqlite3 pf2.db < data/visions.sql
+echo 'data/sizes.sql'
 sqlite3 pf2.db < data/sizes.sql
+echo 'data/traits.sql'
 sqlite3 pf2.db < data/traits.sql
+echo 'data/ancestries.sql'
 sqlite3 pf2.db < data/ancestries.sql
+echo 'data/heritages.sql'
 sqlite3 pf2.db < data/heritages.sql
 
 
