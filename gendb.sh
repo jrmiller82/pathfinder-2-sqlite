a584875802784983f411c30769de958ff4256384
James R Miller <james.ryland.miller@gmail.com>
2019-08-03 22:09:29 -0500

    new file: gendb.sh that is the main database generating script

---
+++ gendb.sh
@@ -0,0 +1,9 @@
+rm pf2.db
+sqlite3 pf2.db < schema/ancestries.sql
+sqlite3 pf2.db < data/visions.sql
+sqlite3 pf2.db < data/sizes.sql
+sqlite3 pf2.db < data/traits.sql
+sqlite3 pf2.db < data/ancestries.sql
+sqlite3 pf2.db < data/heritages.sql
+
+
