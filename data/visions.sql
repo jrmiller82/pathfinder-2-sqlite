b55bd1afca6ede654f8de44bfc50172ca40e94ee
James R Miller <james.ryland.miller@gmail.com>
2019-08-03 22:26:06 -0500

    add smoke vision placeholder on visions.sql

--- data/visions.sql
+++ data/visions.sql
@@ -2,6 +2,8 @@
 
 /* TODO add in any different vision types from Bestiary / Monsters */
 
+/* TODO smoke vision is from young-red dragon */
+
 INSERT INTO visions (vision_id,
                      short_name,
                      description)
@@ -9,6 +11,7 @@ VALUES
   (1, 'Darkvision', 'You can see in darkness and dim light just as well as you can see in bright light, though your vision in darkness is in black and white.'),
   (2, 'Low-Light Vision', 'TODO'),
   (3, 'Keen Eyes', 'TODO'),
-  (4, 'None', 'No special vision.');
+  (4, 'None', 'No special vision.'),
+  (5, 'Smoke Vision', 'TODO');
 
 
