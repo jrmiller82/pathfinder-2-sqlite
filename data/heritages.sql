5ba9fb4444b22f5f143e2e02dd8b430403b978d8
James Miller <james.ryland.miller@gmail.com>
2019-08-06 00:37:23 -0500

    add partial dwarf heritage data

--- data/heritages.sql
+++ data/heritages.sql
@@ -10,14 +10,16 @@
 not to repeat the vision data, but might just have to. It's not like it's
 massive. */
 
+/* TODO decide on how to represent paragraph breaks. */
+
 INSERT INTO heritages (heritage_id,
                        short_name,
                        description)
 VALUES
   (1, 'Ancient-Blooded Dwarf', 'Dwarven heroes of old could shrug off their enemies'' magic, and some of the resistance manifests in you. You gain the Call on Ancient Blood reaction.'),
-  (2, 'Death Warden Dwarf', 'TODO'),
-  (3, 'Forge Dwarf', 'TODO'),
-  (4, 'Rock Dwarf', 'TODO'),
+  (2, 'Death Warden Dwarf', 'Your ancestors have been tomb guardians for generations, and the power they cultivated to ward off necromancy has passed on to you. If you roll a success on a saving throw against a necromancy effect, you get a critical success instead.'),
+  (3, 'Forge Dwarf', 'You have a remarkable adaptation to hot environments from ancestors who inhabited blazing deserts or volcanic chambers beneath the earth. This grants you fire resistance equal to half your level (minimum 1), and you treat environmental heat effects as if they were one step less extreme (incredible heat becomes extreme, extreme heat becomes severe, and so on).'),
+  (4, 'Rock Dwarf', 'Your ancestors lived and worked among the great ancient stones of the mountains or the depths of the earth. This makes you solid as a rock when you plant your feet. You gain a +2 circumstance bonus to your Fortitude or Reflex DC against attempts to Shove or Trip you. This bonus also applies to saving throws against spells or effects that attempt to knock you prone. TODO INSERT PARAGRAH BREAK HERE In addition, if any effect would force you to move 10 feet or more, you are moved only half the distance.'),
   (5, 'Strong-Blooded Dwarf', 'TODO'),
   (6, 'Half-Elf', 'Either one of your parents was an elf, or one or both were half-elves. You have pointed ears and other telltale signs of elf heritage. You gain the elf trait and low-light vision. In addition, you can select elf, half-elf, and human feats whenever you gain an ancestry feat.');
 
