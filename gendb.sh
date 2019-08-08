e249f9446942dea47cd4da022f005aba1720276f
James Miller <james.ryland.miller@gmail.com>
2019-08-07 19:57:23 -0500

    factor out bulk from armor.sql to bulk.sql

--- gendb.sh
+++ gendb.sh
@@ -6,6 +6,7 @@ sqlite3 pf2.db < schema/sources.sql
 sqlite3 pf2.db < schema/backgrounds.sql
 # echo 'schema/abilityscores.sql'
 sqlite3 pf2.db < schema/abilityscores.sql
+sqlite3 pf2.db < schema/bulk.sql
 # echo 'schema/sizes.sql'
 sqlite3 pf2.db < schema/sizes.sql
 # echo 'schema/langs.sql'
@@ -26,6 +27,7 @@ sqlite3 pf2.db < data/sources.sql
 sqlite3 pf2.db < data/backgrounds.sql
 # echo 'data/abilityscores.sql'
 sqlite3 pf2.db < data/abilityscores.sql
+sqlite3 pf2.db < data/bulk.sql
 # echo 'data/senses.sql'
 sqlite3 pf2.db < data/senses.sql
 # echo 'data/sizes.sql'
--- schema/armor.sql
+++ schema/armor.sql
@@ -1,12 +1,3 @@
-CREATE TABLE bulk (
-	bulk_id INTEGER PRIMARY KEY,
-  sources_id INTEGER NOT NULL,
-  sources_pages TEXT NOT NULL,
-	short_name TEXT NOT NULL,
-	long_name TEXT NOT NULL,
-	numerical FLOAT NOT NULL,
-  FOREIGN KEY (sources_id) REFERENCES sources(sources_id)
-);
 
 CREATE TABLE armorgroup (
 	grp_id INTEGER PRIMARY KEY,
