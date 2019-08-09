b4e0fd9cef13832d1bbb6700b2990ae91e6831f5
James Miller <james.ryland.miller@gmail.com>
2019-08-09 14:30:31 -0500

    update sources; still has bug in feats I think

--- schema/sources.sql
+++ schema/sources.sql
@@ -7,7 +7,7 @@ CREATE TABLE sources (
   sources_full_name TEXT NOT NULL UNIQUE,
   sources_short_name TEXT NOT NULL UNIQUE,
   sources_abbrev TEXT NOT NULL UNIQUE,
-  sources_descr TEXT NOT NULL UNIQUE,
+  sources_descr TEXT NOT NULL,
   release_date TEXT NOT NULL, -- in YYYY-MM-DD format
   first_party BOOLEAN NOT NULL,
   ogl_copyright_block TEXT NOT NULL
