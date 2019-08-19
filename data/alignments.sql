382352f41202e39e351c20ffc1b448d3095775d1
James R Miller <james.ryland.miller@gmail.com>
2019-08-18 19:24:30 -0500

    data entered for alignments

---
+++ data/alignments.sql
@@ -0,0 +1,21 @@
+-- -*- mode:sql sql-product:sqlite -*-
+
+PRAGMA foreign_keys = ON;
+
+-- CREATE TABLE alignments (
+--   alignments_id INTEGER PRIMARY KEY,
+--   "name" TEXT UNIQUE NOT NULL, -- 'Lawful Good'
+--   abbr TEXT UNIQUE NOT NULL -- 'LG'
+-- );
+
+BEGIN TRANSACTION;
+INSERT INTO alignments VALUES(1,'Lawful Good','LG');
+INSERT INTO alignments VALUES(2,'Neutral Good','NG');
+INSERT INTO alignments VALUES(3,'Chaotic Good','CG');
+INSERT INTO alignments VALUES(4,'Lawful Neutral','LN');
+INSERT INTO alignments VALUES(5,'Neutral','N');
+INSERT INTO alignments VALUES(6,'Chaotic Neutral','CN');
+INSERT INTO alignments VALUES(7,'Lawful Evil','LE');
+INSERT INTO alignments VALUES(8,'Neutral Evil','NE');
+INSERT INTO alignments VALUES(9,'Chaotic Evil','CE');
+COMMIT;
