50277224ba9fbe18247269ebbbc50eb71e91a683
James R Miller <james.ryland.miller@gmail.com>
2019-08-18 19:32:45 -0500

    fix whitespace on movement.sql

--- data/movements.sql
+++ data/movements.sql
@@ -9,8 +9,8 @@ INSERT INTO movements VALUES(0, 'Land'); -- This is already incorporated into
                                          -- monsters and ancestries but is
                                          -- included here so we have the type of
                                          -- speed
-INSERT INTO movements VALUES(1, 'Burrow'); 
-INSERT INTO movements VALUES(2, 'Climb'); 
-INSERT INTO movements VALUES(3, 'Fly'); 
-INSERT INTO movements VALUES(4, 'Swim'); 
+INSERT INTO movements VALUES(1, 'Burrow');
+INSERT INTO movements VALUES(2, 'Climb');
+INSERT INTO movements VALUES(3, 'Fly');
+INSERT INTO movements VALUES(4, 'Swim');
 COMMIT;
