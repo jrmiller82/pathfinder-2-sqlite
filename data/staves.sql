6d23e8ed2071ed388813ce2089d6f21d369632e3
Brian Haley <brian_haley@live.ca>
2019-08-28 16:26:47 -0400

    Fix staff_id

--- data/staves.sql
+++ data/staves.sql
@@ -16,6 +16,7 @@ INSERT INTO staff (
   "description"
 )
 VALUES
+-- Animal Staff --
 (
 	1,
 	'Animal Staff',
@@ -32,7 +33,7 @@ VALUES
 	"This staff is topped with carved animal and monster heads. While wielding the staff, you gain a +2 circumstance bonus to Nature checks to identify animals." -- https://2e.aonprd.com/Equipment.aspx?ID=349 -- See if they fix <%ACTIONS#197> unsure what it is --
 ),
 (
-	1,
+	2,
 	'Animal Staff (Greater)',
 	8,
 	460,
@@ -47,7 +48,7 @@ VALUES
 	"This staff is topped with carved animal and monster heads. While wielding the staff, you gain a +2 circumstance bonus to Nature checks to identify animals." -- https://2e.aonprd.com/Equipment.aspx?ID=349 -- See if they fix <%ACTIONS#197> unsure what it is --
 ),
 (
-	1,
+	3,
 	'Animal Staff (Major)',
 	12,
 	900,
@@ -61,7 +62,10 @@ VALUES
 	592,
 	"This staff is topped with carved animal and monster heads. While wielding the staff, you gain a +2 circumstance bonus to Nature checks to identify animals." -- https://2e.aonprd.com/Equipment.aspx?ID=349 -- See if they fix <%ACTIONS#197> unsure what it is --
 ),
+-- Mentalist's Staff --
+(
 
+)
 INSERT INTO staff_spell (
   staff_spell_id,
   staff_id,
