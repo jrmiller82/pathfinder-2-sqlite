9f5d077376ea8d26e97a44f698f594a8ab2ccfd5
Brian Haley <brian_haley@live.ca>
2019-08-28 19:14:11 -0400

    Staff of Conjuration

--- data/staves.sql
+++ data/staves.sql
@@ -38,6 +38,13 @@ VALUES
 ,(9, "Staff of Abjuration (Major)", 14, 4000, 1, 'held in one hand', 'Cast a spell', 'You expend a number of charges from the staff to cast a spell from its list.', "Supply one casting of all listed levels of all listed spells.", 1, 594, "This intricately carved wooden staff is warm to the touch and thrums with inner energy. While wielding the staff, you gain a +2 circumstance bonus to checks to identify abjuration magic."
 )
 -- Staff of Conjuration --
+,(10, "Staff of Conjuration", 6, 230, 1, 'held in one hand', 'Cast a spell', 'You expend a number of charges from the staff to cast a spell from its list.', "Supply one casting of all listed levels of all listed spells.", 1, 594, "This ash staff is decorated with animals; wielding it, you gain a +2 circumstance bonus to checks to identify conjuration magic."
+)
+,(11, "Staff of Conjuration (Greater)", 10, 900, 1, 'held in one hand', 'Cast a spell', 'You expend a number of charges from the staff to cast a spell from its list.', "Supply one casting of all listed levels of all listed spells.", 1, 594, "This ash staff is decorated with animals; wielding it, you gain a +2 circumstance bonus to checks to identify conjuration magic."
+)
+,(12, "Staff of Conjuration (Major)", 14, 4000, 1, 'held in one hand', 'Cast a spell', 'You expend a number of charges from the staff to cast a spell from its list.', "Supply one casting of all listed levels of all listed spells.", 1, 594, "This ash staff is decorated with animals; wielding it, you gain a +2 circumstance bonus to checks to identify conjuration magic."
+)
+
 INSERT INTO staff_spell (
   staff_id
   ,"level"
@@ -129,3 +136,46 @@ VALUES
 ,(9, 6, 112)
 ,(9, 6, 368)
 -- Staff of Conjuration --
+,(10, 0, 459)
+,(10, 1, 444)
+,(10, 1, 494)
+,(10, 2, 309)
+,(10, 2, 323)
+,(10, 2, 444)
+,(10, 2, 446)
+-- Staff of Conjuration (Greater) --
+,(11, 0, 459)
+,(11, 1, 444)
+,(11, 1, 494)
+,(11, 2, 309)
+,(11, 2, 323)
+,(11, 2, 444)
+,(11, 2, 446)
+,(11, 3, 432)
+,(11, 3, 444)
+,(11, 3, 446)
+,(11, 4, 79)
+,(11, 4, 232)
+,(11, 4, 444)
+,(11, 4, 446)
+-- Staff of Conjuration (Major) --
+,(11, 0, 459)
+,(11, 1, 444)
+,(11, 1, 494)
+,(11, 2, 309)
+,(11, 2, 323)
+,(11, 2, 444)
+,(11, 2, 446)
+,(11, 3, 432)
+,(11, 3, 444)
+,(11, 3, 446)
+,(11, 4, 79)
+,(11, 4, 232)
+,(11, 4, 444)
+,(11, 4, 446)
+,(11, 5, 37)
+,(11, 5, 444)
+,(11, 5, 446)
+,(11, 6, 323)
+,(11, 6, 444)
+,(11, 6, 446)
