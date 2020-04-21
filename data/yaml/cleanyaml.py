de1765445e6b308d3f08d08e46e1b14c3447ec42
James Miller <james.ryland.miller@gmail.com>
2020-04-21 17:54:30 -0500

    make cleanyaml.py get all yaml files in the directory and then run it

--- data/yaml/cleanyaml.py
+++ data/yaml/cleanyaml.py
@@ -2,27 +2,33 @@
 # TO CLEAN UP AND ORDER ALL THE YAML
 
 import yaml
+import glob
+import os
 
-yfiles = [
-    "actions.yaml", "ancestries.yaml", "armor.yaml", "backgrounds.yaml",
-    "basics.yaml", "bulks.yaml", "conditions.yaml", "damage.yaml",
-    "feats-levels-false-matches.yaml", "feats.yaml", "langs.yaml",
-    "monsters.yaml", "requirements.yaml", "senses.yaml", "skills.yaml",
-    "sources.yaml", "spells.yaml", "traits.yaml", "triggers.yaml"
-]
 
 
 def main():
+
+    # gets all files with a yaml extension in the directory
+    yfiles = []
+    for file in glob.glob("*.yaml"):
+        yfiles.append(file)
+
+    yfiles.sort()
+    print(yfiles)
+
     for x in yfiles:
         print("Doing: {}".format(x))
         with open(x, 'r') as r:
             data = yaml.full_load(r)
             if x == "feats.yaml":
                 for i in data['feat']:
+                    # This is to clean out smart quotes that made it into the
+                    # yaml file so it matches the requirements.yaml
                     if i['requirement'] != None:
-                        print("Before: {}".format(i['requirement']))
+                        # print("Before: {}".format(i['requirement']))
                         i['requirement'] = i['requirement'].replace('’', "'")
-                        print("After: {}".format(i['requirement']))
+                        # print("After: {}".format(i['requirement']))
         final = yaml.safe_dump(data, allow_unicode=True)
         with open(x, 'w') as f:
             f.write(final)
