898b39e3d2110e3ada13ec1669590449fed4a766
James Ryland Miller <james.ryland.miller@gmail.com>
2020-04-22 20:27:19 -0500

    add ac_special: null to monsters.yaml; related to #89

---
+++ data/yaml/deprecated/tmp-ac-special.py
@@ -0,0 +1,25 @@
+import yaml
+
+def main():
+
+    with open('monsters.yaml') as m:
+        data = yaml.full_load(m)
+
+    monsterlist = []
+    for i in data:
+        if 'ac_special' in i:
+            continue
+        else:
+            i['ac_special'] = None
+        monsterlist.append(i)
+
+    finaldata = {'monsters': monsterlist}
+
+    final = yaml.safe_dump(finaldata, allow_unicode=True)
+
+    with open('tmp-monsters.yaml', 'w') as f:
+        f.write(final)
+
+
+if __name__ == "__main__":
+    main()
