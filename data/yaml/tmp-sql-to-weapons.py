6c793f5b48d4cd55bc7ff839687ba95c5a4ac16a
James Miller <james.ryland.miller@gmail.com>
2020-04-23 16:29:38 -0500

    have weapongroups to yaml

--- data/yaml/tmp-sql-to-weapons.py
+++ data/yaml/tmp-sql-to-weapons.py
@@ -3,43 +3,90 @@ import yaml
 import pprint
 
 def main():
-    pp = pprint.PrettyPrinter(indent=2)
+    pp = pprint.PrettyPrinter(indent=4)
     conn = sqlite3.connect('../../pf2.db')
     conn.row_factory = sqlite3.Row
 
+    # DO WEAPON GROUPS
+
     q = """
     SELECT
-    price_gp,
-    dice_size,
-    bulk,
-    hands,
-    range,
-    reload,
+    sources_pages,
     name,
-    descr,
-    (SELECT name FROM actions) as action_name -- use this as template for subqueries
-    FROM weapons;
+    descr
+    FROM weapongroups
     """
 
     c = conn.cursor()
     c.execute(q)
     data = [dict(row) for row in c.fetchall()]
-    for i in data:
-        # handle empty bulk entries to match the abbr field in bulks.yaml
-        if i['bulk'] == '':
-            i['bulk'] = '-'
-        # convert gp prices to cp prices to avoid float issues
-        if i['price_gp'] == '':
-            i['price_gp'] = '0'
-        i['price_cp'] = int(float(i['price_gp']) * 100)
-        del i['price_gp']
-
     pp.pprint(data)
 
-    fdata = {'weapons': data}
-    final = yaml.safe_dump(fdata, allow_unicode=True)
-    with open('tmp-weapons.yaml', 'w') as f:
+            # "source": [
+            #     {
+            #         'abbr': 'CRB',
+            #         'page_start': int(i['sources_pages']),
+            #         'page_stop': int(i['sources_pages'])
+            #     },
+
+    wgdata = []
+    for i in data:
+        res = {
+            'name': i['name'],
+            'descr': i['descr'],
+            'source': [
+                {
+                    'abbr': 'CRB',
+                    'page_start': int(i['sources_pages']),
+                    'page_stop': int(i['sources_pages'])
+                }
+            ]
+        }
+        wgdata.append(res)
+
+    pp.pprint(wgdata)
+    finalwgdata = {'weapongroups': wgdata}
+    pp.pprint(finalwgdata)
+
+    final = yaml.safe_dump(finalwgdata, allow_unicode=True)
+    with open('tmp-weapongroups.yaml', 'w') as f:
         f.write(final)
+
+    # DO WEAPONS
+
+    # q = """
+    # SELECT
+    # price_gp,
+    # dice_size,
+    # bulk,
+    # hands,
+    # range,
+    # reload,
+    # name,
+    # descr,
+    # (SELECT name FROM actions) as action_name -- use this as template for subqueries
+    # FROM weapons;
+    # """
+
+    # c = conn.cursor()
+    # c.execute(q)
+    # data = [dict(row) for row in c.fetchall()]
+    # for i in data:
+    #     # handle empty bulk entries to match the abbr field in bulks.yaml
+    #     if i['bulk'] == '':
+    #         i['bulk'] = '-'
+    #     # convert gp prices to cp prices to avoid float issues
+    #     if i['price_gp'] == '':
+    #         i['price_gp'] = '0'
+    #     i['price_cp'] = int(float(i['price_gp']) * 100)
+    #     del i['price_gp']
+
+    # pp.pprint(data)
+
+    # fdata = {'weapons': data}
+    # final = yaml.safe_dump(fdata, allow_unicode=True)
+    # with open('tmp-weapons.yaml', 'w') as f:
+    #     f.write(final)
 
 
 
